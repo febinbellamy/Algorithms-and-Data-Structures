ee5d05727307a8d568e8828f12c20f526a5b2d32
Febin Bellamy <febinbellamy@gmail.com>
2022-06-22 10:44:21 -0400

    SLL Shift & Unshift

--- Section19_SinglyLinkedLists/SinglyLinkedLists.js
+++ Section19_SinglyLinkedLists/SinglyLinkedLists.js
@@ -1,26 +1,88 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SinglyLinkedList{
-    constructor() {
-        this.head = bull;
-        this.tail = bull;
-        this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // adds a new node to the end of the list
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    push(val) {
-        let newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
+    this.length++;
+    return this;
+  }
+
+  // removes a node from the end of the list & returns it
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  // removes a node from the beginning of the list
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  // adds a new node to the beginning of the list
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new SinglyLinkedList();
+list.push("TestingNode1");
+list.push("TestingNode2");
+list.push("TestingNode3");
+list.push("TestingNode4");
+list.unshift('Testing1');
+list.unshift('Testing2');
+list.unshift('Testing3');
+list.pop();
+list.pop();
+list.shift();
+list.shift();
+
+
