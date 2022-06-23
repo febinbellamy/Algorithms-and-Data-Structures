9d49f5d402ef0c1c08e98a927377f1a5fb42f1e8
Febin Bellamy <febinbellamy@gmail.com>
2022-06-23 08:12:13 -0400

    SLL Insert & Remove

--- 19_SinglyLinkedLists/SinglyLinkedLists.js
+++ 19_SinglyLinkedLists/SinglyLinkedLists.js
@@ -74,9 +74,7 @@ class SinglyLinkedList {
   // retrieves a node by its position in the list
   get(index) {
     if (index < 0 || index >= this.length) return undefined;
-
     let currentNode = this.head;
-
     for (let i = 0; i < index; i++) {
       currentNode = currentNode.next;
     }
@@ -86,12 +84,41 @@ class SinglyLinkedList {
   // reassigns a node's value at a given position in the list
   set(index, val) {
     let specificNode = this.get(index);
-
     if (specificNode) {
       specificNode.val = val;
       return true;
     }
     return false;
+  }
+
+  // adds a new node to the list at a specific position
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (this.length === index) return !!this.push(val);
+
+    let newNode = new Node(val);
+    let prevNode = this.get(index - 1);
+    let placeholderNode = prev.next;
+
+    prevNode.next = newNode;
+    newNode.next = placeholderNode;
+
+    this.length++;
+    return true;
+  }
+
+  // removes a node from the list at a specific position
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let prevNode = this.get(index - 1);
+    let removedNode = prevNode.next;
+    prevNode.next = removedNode.next;
+    this.length--;
+    return removedNode;
   }
 }
 
@@ -109,3 +136,8 @@ list.unshift("Testing2");
 list.unshift("Testing3");
 list.get(1);
 list.get(2);
+list.set(2, "updatedN0D3");
+list.insert(3, "newN0DE1");
+list.insert(4, "newN0DE2");
+list.remove(3);
+list.remove(0);
