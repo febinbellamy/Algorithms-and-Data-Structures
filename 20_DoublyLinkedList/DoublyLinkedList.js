6c7da025708d198b0da232913e6b89e8af1bf378
Febin Bellamy <febinbellamy@gmail.com>
2022-06-24 06:12:55 -0400

    Doubly Linked List Push

---
+++ 20_DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // adds a node to the end of the DLL.
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let DLL = new DoublyLinkedList();
+DLL.push(53);
+DLL.push(41);
+DLL.push(22);
