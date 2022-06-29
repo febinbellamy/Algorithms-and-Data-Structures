d25391d28affd157faf90a95e15bbf1e1c455df0
Febin Bellamy <febinbellamy@gmail.com>
2022-06-29 07:50:08 -0400

    MBH extractMax

--- 24_BinaryHeaps/MaxBH.js
+++ 24_BinaryHeaps/MaxBH.js
@@ -23,6 +23,49 @@ class MaxBinaryHeap {
       childIndex = parentIndex;
     }
   }
+
+  // removing from a max heap
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0;
+    let length = this.values.length;
+    let element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -31,3 +74,4 @@ heap.insert(30);
 heap.insert(14);
 heap.insert(99);
 heap.insert(10);
+heap.extractMax();
