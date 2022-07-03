bb99e3a486bfe187524f6f62b89d07f50ac251a2
Febin Bellamy <febinbellamy@gmail.com>
2022-07-03 07:01:31 -0400

    Graph add vertex & edge

---
+++ 26_Graphs/Graph.js
@@ -0,0 +1,32 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+}
+
+let graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
