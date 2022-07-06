b745d32a9902511b774eb082a5af68db07146980
Febin Bellamy <febinbellamy@gmail.com>
2022-07-06 08:55:28 -0400

    Searching Algorithms

---
+++ 10_SearchingAlgorithms/subStringSearch.js
@@ -0,0 +1,12 @@
+function subStringSearch(longStr, shortStr) {
+  let counter = 0;
+  for (let i = 0; i < longStr.length; i++) {
+    for (let j = 0; j < shortStr.length; j++) {
+      if (shortStr[j] !== longStr[i + j]) break;
+      if (j === shortStr.length - 1) counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(subStringSearch("happy", "appy"));
